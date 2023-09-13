77f850d025084e42a14bf60c5d8d32f78d9e96b5
elloramir <cripboygit@gmail.com>
2023-09-13 14:09:52 -0300

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,89 @@
+<script>
+	import Thumb from './Thumb.svelte';
+</script>
+
+<main>
+	<header class="presentation">
+		<h1>Elloramir</h1>
+		<p>What is a programmer?<br>A miserable little pile of code.</p>
+
+		<nav class="external-links">
+			<a href="https://twitter.com/elloramir" target="_blank">
+				<img alt="icon" src="/images/ladybird.png" />
+			</a>
+			<a href="mailto:ellorakat@gmail.com">
+				<img alt="icon" src="/images/mail.png" />
+			</a>
+			<a href="https://github.com/elloramir" target="_blank">
+				<img alt="icon" src="/images/octopus.png" />
+			</a>
+		</nav>
+	</header>
+
+	<section class="projects">
+		<Thumb project="Perfect Circle" />
+		<Thumb project="Project two asdasdasd asdasdasdasd" />
+		<Thumb project="Hello sailor" />
+		<Thumb project="Hello from no wher asd asd asdasdasdasdasdae" />
+	</section>
+</main>
+
+<div class="credits">
+	<span> Icons by <a target="_blank" href="https://icons8.com">Icons8</a> </span>
+</div>
+
+<style>
+	:global(body) {
+		background-color: rgb(245, 239, 229);
+		color: black;
+	}
+
+	main {
+		width: 100%;
+		max-width: 900px;
+		margin: 0 auto;
+	}
+
+	.presentation {
+		text-align: center;
+	}
+
+	.presentation h1 {
+		font-family: 'Comic Sans MS', cursive, sans-serif;
+		font-size: 3rem;
+	}
+
+	.presentation p {
+		font-family: 'Courier New', Courier, monospace;
+		font-size: 1.2rem;
+	}
+
+	.external-links {
+		display: flex;
+		justify-content: center;
+		gap: 2rem;
+		margin: 30px 0 50px 0;
+	}
+
+	.external-links a {
+		border-bottom: 2px dashed rgba(0, 0, 0, 0.267);
+	}
+
+	.external-links img {
+		width: 80px;
+	}
+
+	.projects {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 10px;
+	}
+
+	/* @NOTE: Since icons8 said we need to, WE NEED TO! */
+	.credits {
+		position: fixed;
+		bottom: 0;
+		right: 0;
+		padding: 10px;
+	}
+</style>
